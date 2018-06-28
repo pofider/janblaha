9570a14bc32af56711f8a929de18b752def9a9ac
pofider <jan.blaha@hotmail.com>
2018-06-28 16:12:36 +0200

    run on tag

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t janblaha .
-docker tag janblaha pofider/janblaha
+docker tag janblaha pofider/janblaha:$TRAVIS_TAG
 docker push pofider/janblaha
