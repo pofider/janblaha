669f1bcfc4a8ad7e5f3e61b951f5bf7b1719833a
pofider <jan.blaha@hotmail.com>
2018-06-30 18:23:31 +0200

    fix working with github

--- docker_push.sh
+++ docker_push.sh
@@ -4,15 +4,16 @@ docker build -t janblaha .
 docker tag janblaha pofider/janblaha:$TRAVIS_TAG
 docker push pofider/janblaha
 
-sudo apt-get install -y ruby
-sudo curl https://hub.github.com/standalone -Lo /usr/bin/hub
-sudo chmod 755 /usr/bin/hub
+sudo add-apt-repository -y ppa:cpick/hub
+sudo apt-get update
+sudo apt-get install -y hub
 
- hub clone "pofider/kubernetes"
- cd kubernetes
+hub config remote.origin.url https://pofider:${GITHUB_TOKEN}@github.com/pofider/kubernetes.git
+hub clone "pofider/kubernetes"
+cd kubernetes
 
 sed -i 's/\$tag/'"$TRAVIS_TAG"'/g' ./kubernetes/janblaha-staging-deployment.yaml
-hub add kubernetes/deployments/janblaha-staging-deployment.yaml
+hub add kubernetes/janblaha-staging-deployment.yaml
 
 hub commit -m "Update the janblaha application"
 
