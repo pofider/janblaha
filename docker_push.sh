07b14a1ace588ff6e7a3864c915102a6d166a56e
pofider <jan.blaha@hotmail.com>
2018-06-30 17:37:56 +0200

    fix git commit

--- docker_push.sh
+++ docker_push.sh
@@ -8,14 +8,12 @@ sudo apt-get install -y ruby
 sudo curl https://hub.github.com/standalone -Lo /usr/bin/hub
 sudo chmod 755 /usr/bin/hub
 
- hub clone "pofder/kubernetes"
+ hub clone "pofider/kubernetes"
  cd kubernetes
 
 sed -i 's/\$tag/'"$TRAVIS_TAG"'/g' ./kubernetes/janblaha-staging-deployment.yaml
 hub add kubernetes/deployments/janblaha-staging-deployment.yaml
 
-hub commit -F- <<EOF
-    Update the janblaha application    
-EOF
+hub commit -m "Update the janblaha application"
 
 hub push origin master
